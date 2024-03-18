5593991558dc73dd037eaa3abdd46258b311e17b
AndikaBastian <andikabastian20@gmail.com>
2024-03-18 11:53:14 +0700

    www

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,5 +5,7 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+$routes->setAutoroute(true);
 $routes->get('/', 'Home::index');
+$routes->get('/tentang', 'home::tentang');
 
